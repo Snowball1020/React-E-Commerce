7ad0c81a6aaf9baae2921a71e9e0a588ca77556a
Yuki <adohiito@gmail.com>
2020-02-11 16:08:42 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {Switch,Route} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar"
+import ProductList from "./components/ProductList"
+import Detail  from "./components/Detail"
+import Cart from "./components/Cart/Cart"
+import Default from "./components/Default"
+import Model from './components/Model'
+
+
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+
+        <Navbar/>
+
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/detail" component={Detail}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
+
+        <Model />
+      </React.Fragment>
+
+    )  
+  }
+}
+
+export default App;
